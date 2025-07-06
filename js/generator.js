720e005e3855d510a9685f994916b6f36fe3c57f
AIMC-dev <aimcmoon@gmail.com>
2025-07-06 22:00:17 +0200

    Create generator.js

---
+++ js/generator.js
@@ -0,0 +1,10 @@
+function generateMeme() {
+  const prompt = document.getElementById('memePrompt').value;
+  const resultDiv = document.getElementById('memeResult');
+  if (!prompt) {
+    resultDiv.innerHTML = '<p style="color:red;">Voer eerst een meme-idee in!</p>';
+    return;
+  }
+  resultDiv.innerHTML = '<p><strong>AI Meme output voor:</strong> ' + prompt + '</p>' +
+    '<img src="https://api.memegen.link/images/custom/_/' + encodeURIComponent(prompt) + '.png?background=none" alt="meme" />';
+}
